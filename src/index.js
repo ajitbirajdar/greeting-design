f879057aa724f28db5b02c194d97380eeebc4e72
ajitbirajdar <ajitbirajdar0@gmail.com>
2022-03-31 12:59:18 +0530

    greeting-design

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "./index.css";
+let curDate = new Date(2021,5,5,5);
+curDate = curDate.getHours();
+let greeting = '';
+const cssStyle ={ 
+ color : 'green'
+};
+
+if(curDate >= 1 && curDate <12){
+  greeting = 'Good Morning';
+  cssStyle.color ='green';
+
+}else if(curDate>=12 && curDate <19){
+  greeting = "Good Afternoon";
+  cssStyle.color ='Orange';
+}else{
+  greeting = "Good Night";
+  cssStyle.color ='Black';
+}
+
+ReactDOM.render(
+<>   
+  <div> 
+
+  <h1>
+    Hello Sir,<span style={cssStyle}> {greeting}</span>
+    </h1>
+    </div>
+    </>,
+  document.getElementById("root")
+);
